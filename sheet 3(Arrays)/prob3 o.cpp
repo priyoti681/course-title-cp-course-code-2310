eab84a80ce7bbd6cdfd622f63c128eb50cf3f0db
priyoti681 <Priyoti320@gmail.com>
2025-06-22 21:42:41 +0600

    Add files via upload

---
+++ sheet 3(Arrays)/prob3 o.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+
+long long index;
+long long a[51];
+a[1]=0;
+a[2]=1;
+for(int i=3;i<=50;i++)
+{
+    a[i]=a[i-1]+a[i-2];
+}
+cin>>index;
+cout<<a[index]<<endl;
+}
